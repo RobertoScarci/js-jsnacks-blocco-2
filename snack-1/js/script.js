ef005ec638b1f69ec09f251449887287083ed3a1
RobertoScarci <robertoscarci.02@gmail.com>
2023-11-17 11:48:33 +0100

    completamento dello snack

---
+++ snack-1/js/script.js
@@ -0,0 +1,14 @@
+// creo l'array vuoto
+const newArray = []
+let somma = 0
+
+while(somma < 50){
+    const userNumber = prompt('inserisci un numero');
+
+    if(!isNaN (userNumber)){
+        somma += userNumber;
+        newArray.push(userNumber);
+    }
+}
+
+console.log(newArray, somma)
